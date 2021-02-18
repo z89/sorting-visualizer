55a850b948822a35a32da8848d88776e40915ab6
Zeo <30657227+z89@users.noreply.github.com>
2021-02-18 17:32:54 +1000

    working finally
    + working bubble sort algo
    + visually updating w/ timing
    + triggered by sort button
    - haven't colorized sorting
    - no other algo's implemented yet

--- app/src/components/Hompage.jsx
+++ app/src/components/Hompage.jsx
@@ -3,61 +3,131 @@ import React, { useState, useEffect } from "react";
 function wait(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+let selected, pressed;
+async function BubbleSort(props) {
+  // for (var j = 0; j < props.length; j++) {
+  //   await wait(100);
+  //   if (props[i] > props[j]) {
+  //     let temp = props[i];
+  //     props[i] = props[j];
+  //     props[j] = temp;
 
-async function BubbleSort(props, i) {
-  await wait(1000);
-  for (var j = 0; j < props.length; j++) {
-    if (props[i] > props[j]) {
+  //     // console.log(props); // rendered each itteration
+  //   }
+  // }
+  for (var i = 0; i < props.length; i++) {
+    document.getElementsByClassName(props[i]).style += { backgroundColor: "green" };
+    await wait(100);
+    if (props[i] > props[i + 1]) {
+      selected = props[i];
       let temp = props[i];
-      props[i] = props[j];
-      props[j] = temp;
-
-      // console.log(props); // rendered each itteration
+      props[i] = props[i + 1];
+      props[i + 1] = temp;
     }
+    console.log(props);
   }
 
   return props;
 }
 
+// backgroundColor: "blue",
+//   //         marginBottom: "10px",
+//   //         width: item + "%",
+//   //         height: "25px",
+
+function Book(props) {
+  const { div, onClick, selectedChoice } = props;
+  console.log(selectedChoice);
+  return (
+    <div
+      style={{
+        backgroundColor: "blue",
+        marginBottom: "10px",
+        width: div + "%",
+        height: "25px",
+      }}
+      onClick={() => {
+        console.log("div clicked");
+      }}
+    >
+      <p>{div}</p>
+    </div>
+  );
+}
+
 const Homepage = () => {
-  let [data, setData] = useState([64, 43, 59, 25, 59, 62, 34, 15, 68]);
+  let [data, setData] = useState([64, 43, 59, 15, 59, 12, 34, 68]);
+  const [selectedAnswer, setAnswer] = useState();
 
   useEffect(() => {
     console.log("array updated");
-    console.log(data);
+    // console.log("selected: " + selected);
+    // console.log(data);
   }, [data]);
-
-  let current = data.map((item, index) => {
-    // console.log(index + ": " + data[index]);
-
-    // console.log();
-    return (
-      <div
-        id={index}
-        style={{ backgroundColor: "blue", marginBottom: "10px", width: item + "%", height: "25px" }}
-        key={index}
-      >
-        {item}
-      </div>
-    );
-  });
 
   return (
     <>
       <h1>Sorting Visualizer</h1>
       <button
         onClick={async () => {
+          console.log("start");
+          pressed = true;
           for (var i = 0; i < data.length; i++) {
             setData([...(await BubbleSort(data, i))]);
+            // document.getElementsByClassName(selected).style.color += "red";
+            console.log(selected);
           }
+          console.log("finished");
+          pressed = false;
         }}
       >
         sort
       </button>
-
-      <div>{current}</div>
+      {data.map((item) => (
+        <Book div={item} onClick={(selectedAnswer) => setAnswer(selectedAnswer)} selectedChoice={selectedAnswer} />
+      ))}
     </>
   );
+
+  // let current = data.map((item, index) => {
+  //   return (
+  //     <div
+  //       className={selected === this.key ? "active" : ""}
+  //       style={{
+  //         backgroundColor: "blue",
+  //         marginBottom: "10px",
+  //         width: item + "%",
+  //         height: "25px",
+  //       }}
+  //       key={index}
+  //     >
+  //       {item}
+  //     </div>
+  //   );
+  // });
+
+  // return (
+  //   <>
+  //     <h1>Sorting Visualizer</h1>
+  //     <button
+  //       onClick={async () => {
+  //         console.log("start");
+  //         pressed = true;
+  //         for (var i = 0; i < data.length; i++) {
+  //           setData([...(await BubbleSort(data, i))]);
+  //           // document.getElementsByClassName(selected).style.color += "red";
+  //           console.log(selected);
+  //         }
+  //         console.log("finished");
+  //         pressed = false;
+  //       }}
+  //     >
+  //       sort
+  //     </button>
+
+  //     <div>{current}</div>
+  //   </>
+  // );
 };
 
 export default Homepage;
